71713788de18e4fe68e63db02958d8f0e87d798c
noof3310 <noof3310@gmail.com>
2022-02-18 12:49:35 +0700

    add route comment

--- routes/comment.js
+++ routes/comment.js
@@ -1,13 +1,49 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
-const { courseCheckMiddleware } = require("../middleware");
+const { studyCheckMiddleware } = require("../middleware");
 
-const course_ip = "";
+const comment_ip = "";
 
-router.get("/", courseCheckMiddleware, async (req, res) => {
+router.get("/", studyCheckMiddleware, async (req, res) => {
     try {
-        const response = await axios.get(course_ip + "/comment", { params: req.query });
+        const response = await axios.get(comment_ip + "/comment", { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/", studyCheckMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(comment_ip + "/comment", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/reply", studyCheckMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(comment_ip + "/comment/reply", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/like", studyCheckMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(comment_ip + "/comment/like", req.body, { params: req.query });
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+router.post("/unlike", studyCheckMiddleware, async (req, res) => {
+    try {
+        const response = await axios.post(comment_ip + "/unlike", req.body, { params: req.query });
         return res.json(response.data);
     } catch (err) {
         return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
