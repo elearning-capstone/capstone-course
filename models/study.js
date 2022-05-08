1fd5175f9a5f4c6067b98f060e645c538ac3a586
noof3310 <noof3310@gmail.com>
2022-05-09 00:31:59 +0700

    add hook for afterUpdate study

--- models/study.js
+++ models/study.js
@@ -1,3 +1,7 @@
+const axios = require("axios");
+
+const course_ip = "http://ip-172-31-36-250.ap-southeast-1.compute.internal:3000";
+
 module.exports = (sequelize, DataTypes) => {
     const study = sequelize.define("study", {
         user_id: {
@@ -25,6 +29,17 @@ module.exports = (sequelize, DataTypes) => {
             foreignKey: "course_id",
         });
     };
+
+    study.afterUpdate(async (study, options) => {
+        try {
+            const user_id = study.dataValues.user_id;
+            const course_id = study.dataValues.course_id;
+            const progression = study.dataValues.progression;
+            await axios.get(course_ip + "/course/checkprogress", { params: { user_id, course_id, progression } });
+        } catch(err) {
+            console.log(err);
+        }
+    });
 
     return study;
 }
